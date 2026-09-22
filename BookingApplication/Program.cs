1f009f9bb42e3d5f4dd48cc65820383cc138761d
Arc-Ender <skacperp@gmail.com>
2026-09-22 13:17:58 +0200

    Implemented runtime compilation

--- BookingApplication/Program.cs
+++ BookingApplication/Program.cs
@@ -4,6 +4,12 @@ using Microsoft.EntityFrameworkCore.Internal;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var mvcBuilder = builder.Services.AddRazorPages();
+
+if (builder.Environment.IsDevelopment())
+{
+    mvcBuilder.AddRazorRuntimeCompilation();
+}
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
