901505da03d32184ab89bc8220e34eebb73ff77e
Arvin Thileepan <arthi1201@oslomet.no>
2026-09-25 00:31:49 +0200

    La til valideringsregler for inputfeltene

--- BookingApplication/Models/RoomSearchViewModel.cs
+++ BookingApplication/Models/RoomSearchViewModel.cs
@@ -1,11 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookingApplication.Models
 {
     public class RoomSearchViewModel
     {
         public DateTime Date { get; set; } = DateTime.Today;
-
+        [Range(typeof(TimeSpan),"09:00","22:00",ErrorMessage = "Study rooms can only be booked between 09:00 and 22:00.")]
         public TimeSpan Time { get; set; } = new TimeSpan(9, 0, 0);
-
+        [Range(1,10,ErrorMessage = "The number of people must be between 1 and 10")]
         public int People { get; set; } = 1;
     }
 }
