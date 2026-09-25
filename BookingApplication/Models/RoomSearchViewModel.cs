47754efd9e5a15f271c6fc6d55c37bbcb9fb268f
Arvin Thileepan <arthi1201@oslomet.no>
2026-09-26 00:50:56 +0200

    La til enda en valideringsregel for tid

--- BookingApplication/Models/RoomSearchViewModel.cs
+++ BookingApplication/Models/RoomSearchViewModel.cs
@@ -2,14 +2,25 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BookingApplication.Models;
 
-public class RoomSearchViewModel
+public class RoomSearchViewModel : IValidatableObject
 {
     public DateTime Date { get; set; } = DateTime.Today;
     [Range(typeof(TimeSpan),"09:00","22:00",ErrorMessage = "Study rooms can only be booked between 09:00 and 22:00.")]
     public TimeSpan StartTime { get; set; } = new TimeSpan(9, 0, 0);
-    [Range(1,10,ErrorMessage = "The number of people must be between 1 and 10")]
+    [Range(typeof(TimeSpan),"09:00","22:00",ErrorMessage = "Study rooms can only be booked between 09:00 and 22:00.")]
     public TimeSpan EndTime { get; set; } = new TimeSpan(10, 0, 0);
 
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+    if(EndTime<=StartTime)
+        {
+            yield return new ValidationResult(
+                "The end time must be after the start time",
+                new[] { nameof(EndTime) }
+            );
+        }    
+    }
+    [Range(1,10,ErrorMessage = "The number of people must be between 1 and 10")]
     public int People { get; set; } = 1;
 
     public List<AvailableRoomViewModel> AvailableRooms { get; set; } = [];
