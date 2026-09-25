b352de30c85e83461e764f4b9e411e44e1c57bdd
Arc-Ender <skacperp@gmail.com>
2026-09-25 12:31:17 +0200

    New data fields for rooms

--- BookingApplication/Models/Room.cs
+++ BookingApplication/Models/Room.cs
@@ -2,6 +2,13 @@ namespace BookingApplication.Models;
 
 public class Room
 {
-  public int Id { get; set; }
-  public String Name { get; set; } = string.Empty;
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public int Capacity { get; set; }
+
+    public string Location { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
 }
