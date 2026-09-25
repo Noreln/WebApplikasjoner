c61f6a819333b7a46f8ac832c38a9f451a3b66a9
Arc-Ender <skacperp@gmail.com>
2026-09-25 12:31:56 +0200

    Updated view model with additional data fields

--- BookingApplication/Models/RoomSearchViewModel.cs
+++ BookingApplication/Models/RoomSearchViewModel.cs
@@ -1,11 +1,15 @@
-namespace BookingApplication.Models
+namespace BookingApplication.Models;
+
+public class RoomSearchViewModel
 {
-    public class RoomSearchViewModel
-    {
-        public DateTime Date { get; set; } = DateTime.Today;
+    public DateTime Date { get; set; } = DateTime.Today;
 
-        public TimeSpan Time { get; set; } = new TimeSpan(9, 0, 0);
+    public TimeSpan StartTime { get; set; } = new TimeSpan(9, 0, 0);
 
-        public int People { get; set; } = 1;
-    }
+    public TimeSpan EndTime { get; set; } = new TimeSpan(10, 0, 0);
+
+    public int People { get; set; } = 1;
+
+    public List<AvailableRoomViewModel> AvailableRooms { get; set; } = [];
 }
+
